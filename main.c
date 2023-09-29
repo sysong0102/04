a6d3cad6bc50cc064b5eecc665b80a9fa0d9c47d
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-09-29 20:17:03 +0900

    Q2

--- main.c
+++ main.c
@@ -5,16 +5,16 @@
 
 int main(int argc, char *argv[]) 
 {
-	int x, y, z, m;
-	int a, b, c;
-	x = 2;
-	z = 1;
-	a = 3;
-	b = 4;
-	c = 5;
-	y = a * x * x + b * x + c;
-	m = (x + y + z)/3;
-	printf("y = %d, m = %d", y, m);
+	int a, b;
+	printf("input two integers : ");
+	scanf("%i %i", &a, &b);
+	
+	printf("%i + %i = %i \n", a, b, a+b);
+	printf("%i - %i = %i \n", a, b, a-b);
+	printf("%i * %i = %i \n", a, b, a*b);
+	printf("%i / %i = %i \n", a, b, a/b);
+	printf("%i %% %i = %i \n", a, b, a%b);
+	
 	system("PAUSE");
 	return 0;
 }
