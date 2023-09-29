91554a267c1a049f1fbad07e7f680f9d4361717d
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-09-29 20:38:14 +0900

    Q6

--- main.c
+++ main.c
@@ -5,21 +5,12 @@
 
 int main(int argc, char *argv[]) 
 {
-	unsigned int x;
-	int b;
+	int a;
 	
-	printf("input a number :");
-	scanf("%ui", &x);
+	printf("input the second : ");
+	scanf("%i", &a);
 	
-	for (b=0; x!=0; x >>= 1)
-	{
-		if (x & 1)
-		{
-			b++;
-		}
-	}
-	
-	printf("The result is : %i\n", b);
+	printf("The time for %i second is %i : %i : %i\n", a, a/3600, (a%3600)/60, (a%3600)%60);
 	
 	system("PAUSE");
 	return 0;
